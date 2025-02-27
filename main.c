f0b7cbf067290225971716bfd1702f616060d360
Martin Hanna <martinhanna002@gmail.com>
2025-02-27 23:49:48 +0400

    Update main.c
    Finished function to read data. Created function to choose random word from array.

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib>
 #include <string.h>
+#include <random.h> // ?
+#include <time.h> // ?
 #define FILE_NAME "wordlist.txt"
 using namespace std;
 
@@ -22,9 +24,24 @@ chr = getc(file);
 }
 rewind(file);
 {
-for (int i = 0 ; i < num_lines ; i++) {
-char* words[i] = NULL;
+char* words[num_lines];
+size_t n; // all lines are 5 letter words, so this can be optimized
+for (size_t i = 0 ; i < num_lines ; i++) {
+words[i] = NULL;
+size_t n = 0;
+
+getline(&words[i], &n, file);
+  // add check for error getting line
 }
+}
+return words;
 }
 
+char* /*???*/ chooseRandomWord(char* words, int number_words) {
+int random_index = rand() % number_words;
+return words[random_index];
+}
+
+int main(void) {
+  return 0;
 }
